1d9d159773a08e355b63cbddab0bd78a8b5ece6e
Avinash Tag <jagdishtag@jagdishs-macbook-pro.local>
2016-01-18 22:31:23 +0530

    Initial Commit

---
+++ ZZPings/ZZPings.h
@@ -0,0 +1,19 @@
+//
+//  ZZPings.h
+//  ZZPings
+//
+//  Created by Jagdish Tag on 18/01/16.
+//  Copyright © 2016 Avinash Tag. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ZZPings.
+FOUNDATION_EXPORT double ZZPingsVersionNumber;
+
+//! Project version string for ZZPings.
+FOUNDATION_EXPORT const unsigned char ZZPingsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZZPings/PublicHeader.h>
+
+
